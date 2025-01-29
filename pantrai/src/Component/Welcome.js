0be2124f908864b159d76cc12a723ab0f576dbc6
Vishnu Manoj <vishman2001@mac.home>
2025-01-29 03:35:03 -0500

    react app and welcome header created

---
+++ pantrai/src/Component/Welcome.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../Styles/welcome.css';
+const Welcome = () => {
+    return (
+        <div class = 'outer'>
+            <div class='inner'>
+                <h1>Welcome to your <span class='title'>PantrAI</span></h1>
+            </div>
+        </div>
+    );
+};
+
+export default Welcome;
